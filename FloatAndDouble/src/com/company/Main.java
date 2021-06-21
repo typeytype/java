9650c1eab2e7473ad74b4f3ae1c844072d860c2c
typeytype <mrmontyhall95@gmail.com>
2021-06-21 14:12:42 +0100

    Getting Started 1

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float minimum value = " + myMaxFloatValue);
+        
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double minimum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("MyIntValue= "+ myIntValue);
+        System.out.println("MyFloatValue= "+ myFloatValue);
+        System.out.println("MyDoubleValue= "+ myDoubleValue);
+
+        // Challenge Conversion Pounds to Kilograms
+        double pounds = 100;
+        double kilograms = pounds * 0.45359237;
+        System.out.println(kilograms + "kg");
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+    }
+}
