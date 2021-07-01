b799a51c263155abc38b307809649483258f8b11
typeytype <mrmontyhall95@gmail.com>
2021-07-01 15:16:25 +0100

    First Steps

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -45,6 +45,44 @@ public class Main {
             System.out.println("And I am scared of aliens");
         }
 
-        
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) { // && represents and
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) { // remember to use two equals, otherwise error due to only assigning of value
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar) { // this shorthand represents a boolean test for true/false depending on the exclamation mark
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false; // ternary operator ? conditionally assigns one of two values
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        double chValue1 = 20;
+        double chValue2 = 80;
+        double chValue3 = ((chValue1 + chValue2) * 100);
+        double chRemain = chValue3 % 40;
+        boolean zeroCheck = (chRemain == 0 ? true : false);
+        System.out.println(zeroCheck);
+        if (zeroCheck == false) {
+            System.out.println("Got some remainder");
+        }
     }
 }
