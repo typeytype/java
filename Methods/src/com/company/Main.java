1d12217502872c08288f45eb1e711db56810d373
typeytype <mrmontyhall95@gmail.com>
2021-07-02 13:11:52 +0100

    If

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,35 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int mainScore = 800;
+
+        calculateInitScore(800);
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score " + highScore);
+        calculateScore(true, 10000, 8, 200);
+
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore+=2000;
+            return finalScore;
+        } else { return -1; }
+    }
+
+    public static void calculateInitScore(int mainScore) {
         if(mainScore < 5000 && mainScore > 1000) {
             System.out.println("Your score was less than 5000 but more than 1000.");
             System.out.println("The if was executed");
         } else if (mainScore < 1000) {
             System.out.println("Your score was less than 1000.");
             System.out.println("The elseif was executed.");
-        }
-        else {
+        } else {
             System.out.println("The else was executed.");
-        }
-
-
-        if (gameOver == true) {
-            int score = 800;
-            int levelCompleted = 5;
-            int bonus = 100;
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore+=1000;
-            System.out.println("Your final score " + finalScore);
-
-        }
-        if (gameOver == true) {
-            int score = 10000;
-            int levelCompleted = 8;
-            int bonus = 200;
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore+=1000;
-            System.out.println("Your final score " + finalScore);
         }
     }
 }
