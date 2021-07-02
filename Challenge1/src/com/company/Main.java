209f99478dec50cf2ff429d3cfcd2a7dadbc70b7
typeytype <mrmontyhall95@gmail.com>
2021-07-02 15:29:28 +0100

    UnitConversion

---
+++ Challenge1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import static java.lang.Math.round;
+public class Main {
+
+    public static void main(String[] args) {
+
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+
+    }
+
+
+    }
---
+++ Challenge1/src/com/company/SpeedConverter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if(kilometersPerHour<0){return -1;}
+        else {
+            return Math.round(kilometersPerHour / 1.609);}}
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour<0) {System.out.println("Invalid Value");
+        }else{long mphResult = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + "km/h = " + mphResult + " mi/h");
+        }}
+}
